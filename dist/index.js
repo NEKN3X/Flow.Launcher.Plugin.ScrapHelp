2325b5224baab4fa4401f9053f94e507e6df902b
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-06-20 12:53:57 +0900

    fix error

--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,36 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import * as rpc from "vscode-jsonrpc/node.js";
 const connection = rpc.createMessageConnection(new rpc.StreamMessageReader(process.stdin), new rpc.StreamMessageWriter(process.stdout));
-connection.onRequest("initialize", (params) => {
+connection.onRequest("initialize", (params) => __awaiter(void 0, void 0, void 0, function* () {
     return;
-});
-connection.onRequest("query", (query, settings) => {
+}));
+connection.onRequest("query", (query, settings) => __awaiter(void 0, void 0, void 0, function* () {
     return {
         result: [
             {
                 title: query["search"],
-                subtitle: "Open Google",
+                subtitle: "Search result for: " + query["search"],
                 jsonRPCAction: {
                     method: "open_url",
-                    parameters: [],
+                    parameters: [
+                        "https://www.google.com/search?q=" +
+                            encodeURIComponent(query["search"]),
+                    ],
                 },
             },
         ],
     };
-});
-connection.onRequest("open_url", (params) => {
-    connection.sendRequest("OpenUrl", { url: "https://www.google.com" });
+}));
+connection.onRequest("open_url", (params) => __awaiter(void 0, void 0, void 0, function* () {
+    yield connection.sendRequest("OpenUrl", { url: "https://www.google.com" });
     return;
-});
+}));
 connection.listen();
--- src/index.ts
+++ src/index.ts
@@ -5,28 +5,31 @@ const connection = rpc.createMessageConnection(
   new rpc.StreamMessageWriter(process.stdout)
 );
 
-connection.onRequest("initialize", (params) => {
+connection.onRequest("initialize", async (params) => {
   return;
 });
 
-connection.onRequest("query", (query, settings) => {
+connection.onRequest("query", async (query, settings) => {
   return {
     result: [
       {
         title: query["search"],
-        subtitle: "Open Google",
+        subtitle: "Search result for: " + query["search"],
         jsonRPCAction: {
           method: "open_url",
-          parameters: [],
+          parameters: [
+            "https://www.google.com/search?q=" +
+              encodeURIComponent(query["search"]),
+          ],
         },
       },
     ],
   };
 });
 
-connection.onRequest("open_url", (params): void => {
-  connection.sendRequest("OpenUrl", { url: "https://www.google.com" });
-  return;
+connection.onRequest("open_url", async (params) => {
+  await connection.sendRequest("OpenUrl", { url: "https://www.google.com" });
+  return {};
 });
 
 connection.listen();
