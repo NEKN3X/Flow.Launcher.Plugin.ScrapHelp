dff6411ef8244c1fbdc936f733275557d1fdfb65
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-06-20 20:09:13 +0900

    Add new asset images: file-code.png, globe.png, and sticky-note.png
    - Added file-code.png with 737 bytes of PNG data.
    - Added globe.png with 1368 bytes of PNG data.
    - Added sticky-note.png with 594 bytes of PNG data.

--- src/plugins/makeResult.ts
+++ src/plugins/makeResult.ts
@@ -1,22 +1,23 @@
 import { SearchHelpResult } from "../domain/getAllHelp.js";
 
-export async function makeResult(
-  help: SearchHelpResult
-): Promise<ResultItem[]> {
+export async function makeResult(help: SearchHelpResult) {
   return help.flatMap((item) => {
-    return item.pages.flatMap((page) => {
-      return {
-        title: page.title,
-        subTitle: `/${item.project}`,
-        jsonRPCAction: {
-          method: "open_url",
-          parameters: [
-            `https://scrapbox.io/${item.project}/${encodeURIComponent(
-              page.title
-            )}`,
-          ],
+    return item.pages.flatMap((page): ResultItem[] => {
+      return [
+        {
+          title: page.title,
+          subTitle: `/${item.project}`,
+          icoPath: "assets/sticky-note.png",
+          jsonRPCAction: {
+            method: "open_url",
+            parameters: [
+              `https://scrapbox.io/${item.project}/${encodeURIComponent(
+                page.title
+              )}`,
+            ],
+          },
         },
-      };
+      ];
     });
   });
 }
--- src/plugins/types.d.ts
+++ src/plugins/types.d.ts
@@ -1,6 +1,11 @@
 type ResultItem = {
   title: string;
   subTitle?: string;
+  glyph?: {
+    glyph: string;
+    fontFamily: string;
+  };
+  icoPath?: string;
   jsonRPCAction: {
     method: string;
     parameters: any[];
