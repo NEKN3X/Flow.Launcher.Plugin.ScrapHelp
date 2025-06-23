6bab17b60af6dc0939ab01ad0447be5c55c241d7
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-06-24 00:35:51 +0900

    Revert "Remove example.test.ts"
    This reverts commit 112f506210a0d6ae0bc32cdca8de322f3b386222.

---
+++ src/example.test.ts
@@ -0,0 +1,11 @@
+import { expect } from 'vitest'
+
+it('responds with the user', async () => {
+  const response = await fetch('https://api.example.com/user')
+
+  await expect(response.json()).resolves.toEqual({
+    id: 'abc-123',
+    firstName: 'John',
+    lastName: 'Maverick',
+  })
+})
