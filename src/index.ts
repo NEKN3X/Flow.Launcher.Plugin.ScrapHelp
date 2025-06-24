d6d4dbc2259c5efb83414c0e1b325b9a817b8b51
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-06-24 20:49:56 +0900

    Refactor glossary handling: replace getGlossary function with extractGlossary usage and update query method to improve glossary extraction logic

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import type { AxiosCacheInstance } from 'axios-cache-interceptor'
 import type { Context, Methods, Query, ResultItem, Settings } from './types.js'
 import process from 'node:process'
+import { extractGlossary } from '@core/help/extractGlossary.js'
 import { expandHelpfeel } from '@core/help/parser.js'
 import { client } from '@shell/api/client.js'
 import { createGetScrapboxPages } from '@shell/api/getScrapboxPages.js'
-import { getGlossary } from '@shell/getGlossary.js'
 import { searchResult } from '@shell/searchResult.js'
 import { setupCache } from 'axios-cache-interceptor'
 import * as rpc from 'vscode-jsonrpc/node.js'
@@ -30,73 +30,81 @@ const methods: Methods[] = [
     handler: async (query: Query, settings: Settings) => {
       const getScrapboxPages = createGetScrapboxPages(cacheClient, context.currentPluginMetadata.pluginCacheDirectoryPath)
       const projects = settings.projects.split(',')
-      const glossary = await getGlossary(context.currentPluginMetadata.pluginCacheDirectoryPath, settings.glossary)
+      let glossary = new Map<string, string>([])
       const result: ResultItem[] = (await Promise.all(
-        projects.map(project => (
-          getScrapboxPages(project, settings.sid).then(pages => pages.flatMap((page): ResultItem[] => ([
-            {
-              title: page.title,
-              subTitle: `${project}/${page.title}`,
-              jsonRPCAction: {
-                method: 'open_url',
-                parameters: [new URL(`https://scrapbox.io/${project}/${page.title}`)],
-              },
-            },
-            ...page.help.flatMap((help): ResultItem[] => {
-              switch (help.type) {
-                case 'scrapbox_page':
-                  return [
-                    {
-                      title: help.helpfeel,
-                      subTitle: `${help.project}/${help.title}`,
-                      jsonRPCAction: {
-                        method: 'open_url',
-                        parameters: [new URL(`https://scrapbox.io/${help.project}/${help.title}`)],
-                      },
-                    },
-                  ]
-                case 'web_page':
-                { const url = new URL(help.url)
-                  return [
-                    {
-                      title: help.helpfeel,
-                      subTitle: `${url.hostname}${url.pathname}`,
-                      jsonRPCAction: {
-                        method: 'open_url',
-                        parameters: [url],
-                      },
-                    },
-                  ] }
-                case 'text':
-                  return [
-                    {
-                      title: help.helpfeel,
-                      subTitle: help.text,
-                      jsonRPCAction: {
-                        method: 'copy_text',
-                        parameters: [help.text],
-                      },
-                    },
-                  ]
-                case 'file':
-                  return [
-                    {
-                      title: help.helpfeel,
-                      subTitle: help.fileName,
-                      jsonRPCAction: {
-                        method: 'copy_text',
-                        parameters: [help.fileName],
-                      },
-                    },
-                  ]
-                default:
-                  return []
+        projects.map((project, index) => (
+          getScrapboxPages(project, settings.sid).then(async (pages) => {
+            if (index === 0) {
+              const glossaryPage = pages.find(page => page.title === 'Glossary')
+              if (glossaryPage) {
+                glossary = extractGlossary(glossaryPage.lines.map(line => line.text))
               }
-            }).flatMap(help => (expandHelpfeel(help.title, glossary).map(expandedHelpfeel => ({
-              ...help,
-              title: expandedHelpfeel,
-            })))),
-          ])),
+            }
+            return pages.flatMap((page): ResultItem[] => ([
+              {
+                title: page.title,
+                subTitle: `${project}/${page.title}`,
+                jsonRPCAction: {
+                  method: 'open_url',
+                  parameters: [new URL(`https://scrapbox.io/${project}/${page.title}`)],
+                },
+              },
+              ...page.help.flatMap((help): ResultItem[] => {
+                switch (help.type) {
+                  case 'scrapbox_page':
+                    return [
+                      {
+                        title: help.helpfeel,
+                        subTitle: `${help.project}/${help.title}`,
+                        jsonRPCAction: {
+                          method: 'open_url',
+                          parameters: [new URL(`https://scrapbox.io/${help.project}/${help.title}`)],
+                        },
+                      },
+                    ]
+                  case 'web_page':
+                  { const url = new URL(help.url)
+                    return [
+                      {
+                        title: help.helpfeel,
+                        subTitle: `${url.hostname}${url.pathname}`,
+                        jsonRPCAction: {
+                          method: 'open_url',
+                          parameters: [url],
+                        },
+                      },
+                    ] }
+                  case 'text':
+                    return [
+                      {
+                        title: help.helpfeel,
+                        subTitle: help.text,
+                        jsonRPCAction: {
+                          method: 'copy_text',
+                          parameters: [help.text],
+                        },
+                      },
+                    ]
+                  case 'file':
+                    return [
+                      {
+                        title: help.helpfeel,
+                        subTitle: help.fileName,
+                        jsonRPCAction: {
+                          method: 'copy_text',
+                          parameters: [help.fileName],
+                        },
+                      },
+                    ]
+                  default:
+                    return []
+                }
+              }).flatMap(help => (expandHelpfeel(help.title, glossary).map(expandedHelpfeel => ({
+                ...help,
+                title: expandedHelpfeel,
+              })))),
+            ]))
+          },
           )),
         ),
       )).flat()
